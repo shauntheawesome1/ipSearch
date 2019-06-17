4435bff5825b5b7ef480dd4a52d0af8df50ccfd0
Shaun Munshi <Shaun.munshi@gmail.com>
2019-06-17 12:02:51 -0700

    Update ipSearch.sh

--- ipSearch.sh
+++ ipSearch.sh
@@ -2,7 +2,7 @@
 typeset -i a=$1
 typeset -i b=$2
 typeset -i c=$3
-for i in {2..254}
+for i in {2..254}    # 0 is non usable, 1 is default gateway, 255 is broadcast
         do
                 typeset -i x=$i
                 ping -c 1 $a.$b.$c.$x > newFile.txt
